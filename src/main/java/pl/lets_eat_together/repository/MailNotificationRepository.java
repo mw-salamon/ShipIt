fe3c0d2271d9cbfdbbd2cb765ccdb80b66881b9a
Karolina606 <nogacka.karolina@gmail.com>
2022-10-08 20:22:05 +0200

    Merge branch 'development' of https://github.com/Ezqull/ShipIt into feature/issue#3_2
    # Conflicts:
    #	src/main/java/pl/lets_eat_together/model/MailNotification.java

--- src/main/java/pl/lets_eat_together/repository/MailNotificationRepository.java
+++ src/main/java/pl/lets_eat_together/repository/MailNotificationRepository.java
@@ -2,10 +2,10 @@ package pl.lets_eat_together.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import pl.lets_eat_together.model.MailNotification;
+import pl.lets_eat_together.model.Notification;
 
 
 @Repository("mailNotificationRepository")
-public interface MailNotificationRepository extends JpaRepository<MailNotification, Long> {
+public interface MailNotificationRepository extends JpaRepository<Notification, Long> {
 
 }
