362efe92eccf44bae9b0795f0016acc6836246e3
Karolina606 <nogacka.karolina@gmail.com>
2022-10-09 16:59:50 +0200

    correct

--- src/main/java/pl/lets_eat_together/repository/OrderRepository.java
+++ src/main/java/pl/lets_eat_together/repository/OrderRepository.java
@@ -13,11 +13,5 @@ import java.util.List;
 public interface OrderRepository extends JpaRepository<Order, Long> {
     public List<Order> findAllByOrderByCallDeadlineDesc();
 
-//    @Query("select * from orders o where o.status=0 order by o.call_deadline DESC")
-//    public List<Order> findAllOpen();
-//
-//    @Query("select * from orders o where o.status=3 or o.status=4 order by o.call_deadline DESC")
-//    public List<Order> findAllClosedOrCancelled();
-
     public List<Order> findAllByStatusOrderByCallDeadlineDesc(Status status);
 }
